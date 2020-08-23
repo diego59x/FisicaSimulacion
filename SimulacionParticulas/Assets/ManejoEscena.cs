add5f8e00da2f46e62c72d688fcec3ea3443c2c2
diego59x <diegoalvarez1237@hotmail.com>
2020-08-23 12:30:27 -0600

    primera partee

---
+++ SimulacionParticulas/Assets/ManejoEscena.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManejoEscena : MonoBehaviour
+{
+    
+    public void SimulacionScene()
+    {
+        SceneManager.LoadScene("Simulacion");
+    }
+
+    public void FirstScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
