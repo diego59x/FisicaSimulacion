298463aa12e43af272aa01cb16346ce90aee0926
diego59x <diegoalvarez1237@hotmail.com>
2020-09-02 22:40:44 -0600

    se arreglaron errores de calculo y se añadieron otras particulas, hay errores cuando se ingresan los datos

---
+++ Simulacion/Assets/Scripts/ManejoEscena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManejoEscena : MonoBehaviour
+{
+
+    public void FirstScene()
+    {
+        SceneManager.LoadScene("Principal");
+    }
+    public void SecondScene()
+    {
+        SceneManager.LoadScene("Simulacion");
+    }
+}
